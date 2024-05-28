4ad278eaa0e83c81d5d1a944073192130f963dac
ShubhamTiwari55 <stofficial059@gmail.com>
2024-05-28 15:02:00 +0530

    env file added

--- ticket-tracker/src/App.js
+++ ticket-tracker/src/App.js
@@ -1,14 +1,11 @@
-//import { app } from "./firebase";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-  
       </header>
     </div>
   );
 }
-
-export default App;
+  export default App;
--- ticket-tracker/src/firebase.js
+++ ticket-tracker/src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvJ1x6C87PQuF2Z24odsEXQQ5Ju4RSFT0", // API key for authenticating requests
-  authDomain: "ticket-tracker-2c78a.firebaseapp.com", // Auth domain for Firebase Auth
-  projectId: "ticket-tracker-2c78a", // Project ID for your Firebase project
-  storageBucket: "ticket-tracker-2c78a.appspot.com", // Storage bucket for Firebase Storage
-  messagingSenderId: "588965340658", // Sender ID for Firebase Cloud Messaging
-  appId: "1:588965340658:web:3edd7467e69673dd7f776b", // App ID for the Firebase project
+  apiKey: process.env.APIKEY, // API key for authenticating requests
+  authDomain: process.env.AUTH_DOMAIN, // Auth domain for Firebase Auth
+  projectId: process.env.PROJECT_ID, // Project ID for your Firebase project
+  storageBucket: process.env.STORAGE_BUCKET, // Storage bucket for Firebase Storage
+  messagingSenderId: process.env.MESSAGE_SENDER_ID, // Sender ID for Firebase Cloud Messaging
+  appId: process.env.APP_ID, // App ID for the Firebase project
   databaseURL: "https://ticket-tracker-2c78a-default-rtdb.firebaseio.com" // Database URL for Firebase Realtime Database
 };
 
