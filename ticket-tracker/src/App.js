cf2f269ef1f5343afd8ce365460e7c449a43dd7b
ShubhamTiwari55 <stofficial059@gmail.com>
2024-05-28 10:39:26 +0530

    changes done

--- ticket-tracker/src/App.js
+++ ticket-tracker/src/App.js
@@ -1,23 +1,11 @@
-import {getDatabase, ref, set} from "firebase/database";
-import { app } from "./firebase";
+//import { app } from "./firebase";
 import './App.css';
 
-const db = getDatabase(app);
-
 function App() {
-  //Testing for firebase connection and database 
-  // const putData = () => {
-  //   set(ref(db, "users/shubham"),{
-  //     id: 1,
-  //     name: "Shubham",
-  //     age: 20,
-  //   });
-  // };
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Hey there!</h1>
-        {/* <button onClick={putData}>Put data</button> */}
+  
       </header>
     </div>
   );
--- ticket-tracker/src/firebase.js
+++ ticket-tracker/src/firebase.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvJ1x6C87PQuF2Z24odsEXQQ5Ju4RSFT0",
-  authDomain: "ticket-tracker-2c78a.firebaseapp.com",
-  projectId: "ticket-tracker-2c78a",
-  storageBucket: "ticket-tracker-2c78a.appspot.com",
-  messagingSenderId: "588965340658",
-  appId: "1:588965340658:web:3edd7467e69673dd7f776b",
-  databaseURL: "https://ticket-tracker-2c78a-default-rtdb.firebaseio.com"
+  apiKey: "AIzaSyBvJ1x6C87PQuF2Z24odsEXQQ5Ju4RSFT0", // API key for authenticating requests
+  authDomain: "ticket-tracker-2c78a.firebaseapp.com", // Auth domain for Firebase Auth
+  projectId: "ticket-tracker-2c78a", // Project ID for your Firebase project
+  storageBucket: "ticket-tracker-2c78a.appspot.com", // Storage bucket for Firebase Storage
+  messagingSenderId: "588965340658", // Sender ID for Firebase Cloud Messaging
+  appId: "1:588965340658:web:3edd7467e69673dd7f776b", // App ID for the Firebase project
+  databaseURL: "https://ticket-tracker-2c78a-default-rtdb.firebaseio.com" // Database URL for Firebase Realtime Database
 };
 
-// Initialize Firebase
+// Initialize Firebase with the provided configuration
 export const app = initializeApp(firebaseConfig);
