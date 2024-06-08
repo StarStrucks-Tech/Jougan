8728091bb811edd73f88c94c8674f572b5942c8b
Shradha Jha <114392601+shradhaj06@users.noreply.github.com>
2024-06-08 18:05:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './Screens/Home/Home';
-import TicketDetails from './Screens/TicketDetails/TicketDetails';
-import Sidebar from './Components/Sidebar/Sidebar';
+import Home from './screens/Home/Home';
+import TicketDetails from './screens/TicketDetails/TicketDetails';
+import Sidebar from './components/Sidebar/Sidebar';
 import { ErrorProvider } from './contexts/ErrorContext';
-import ErrorModal from './Components/ErrorModal/ErrorModal';
+import ErrorModal from './components/ErrorModal/ErrorModal';
 
+/**
+ * App component that sets up the main application structure.
+ * Wraps the application in the ErrorProvider and sets up routing.
+ * @returns {JSX.Element} The main app element
+ */
 function App() {
   return (
     <ErrorProvider>
+      <Router>
         <div className="app">
           <Sidebar />
           <div className="main-content">
@@ -22,7 +28,7 @@ function App() {
           </div>
           <ErrorModal />
         </div>
-      
+      </Router>
     </ErrorProvider>
   );
 }
