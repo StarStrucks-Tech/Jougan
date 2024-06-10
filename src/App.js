14cc037f0f1f3966ba29de37ad5180d5132d7f42
Shradha Jha <shradhajha0609@gmail.com>
2024-06-10 21:36:26 +0530

    integrated infocollector modal

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import './App.css';
 import Home from './Screens/Home/Home';
 import TicketDetails from './Screens/TicketDetails/TicketDetails';
 import Sidebar from './Components/Sidebar/Sidebar';
-
+import { ErrorProvider } from './contexts/ErrorContext';
+import ErrorModal from './Components/ErrorModal/ErrorModal';
 function App() {
   return (
+    <ErrorProvider>
       <div className="app">
         <Sidebar />
         <div className="main-content">
@@ -17,7 +19,10 @@ function App() {
             <Route path="/" element={<Home />} />
           </Routes>
         </div>
+        <ErrorModal />
       </div>
+   
+  </ErrorProvider>
 
   );
 }
--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -1,14 +1,19 @@
 // src/screens/Home/Home.jsx
-import React from 'react';
+import React, { useState } from 'react';
+import InfoCollectorModal from '../../Components/InfoCollectorModal/InfoCollectorModal';
 /**
  * Home component that serves as the home page.
  * @returns {JSX.Element} The home page element
  */
 const Home = () => {
-
+  const [isInfoModalVisible, setInfoModalVisible] = useState(true);
   return (
     <div>
       <h1>Home</h1>
+      <InfoCollectorModal
+            isVisible={isInfoModalVisible}
+            onSuccess={() => setInfoModalVisible(false)}
+          />
      
     </div>
   );
