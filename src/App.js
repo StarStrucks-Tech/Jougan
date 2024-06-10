2abd8c32a192d02e6e1a4ea4082eb2e373d37097
Shradha Jha <shradhajha0609@gmail.com>
2024-06-10 11:43:36 +0530

    reverted changes of appBar and adjusted sidebar's css

--- src/App.js
+++ src/App.js
@@ -5,32 +5,20 @@ import './App.css';
 import Home from './Screens/Home/Home';
 import TicketDetails from './Screens/TicketDetails/TicketDetails';
 import Sidebar from './Components/Sidebar/Sidebar';
-import { ErrorProvider } from './contexts/ErrorContext';
-import ErrorModal from './Components/ErrorModal/ErrorModal';
 
-/**
- * App component that sets up the main application structure.
- * Wraps the application in the ErrorProvider and sets up routing.
- * @returns {JSX.Element} The main app element
- */
 function App() {
   return (
-    <ErrorProvider>
-
-        <div className="app">
-          <Sidebar />
-          <div className="main-content">
-            <Routes>
-              <Route path="/home" element={<Home />} />
-              <Route path="/ticket" element={<TicketDetails />} />
-              <Route path="/" element={<Home />} />
-            </Routes>
-          </div>
-          <ErrorModal />
+      <div className="app">
+        <Sidebar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/ticket" element={<TicketDetails />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
         </div>
+      </div>
 
-    </ErrorProvider>
   );
 }
-
 export default App;
--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -1,26 +1,15 @@
 // src/screens/Home/Home.jsx
 import React from 'react';
-import { useError } from '../../contexts/ErrorContext';
-
 /**
  * Home component that serves as the home page.
- * Includes a button to trigger an error for demonstration purposes.
  * @returns {JSX.Element} The home page element
  */
 const Home = () => {
-  const { toggleErrorState } = useError();
-
-  /**
-   * Handle button click to trigger an error.
-   */
-  const handleClick = () => {
-    toggleErrorState('This is a test error message.');
-  };
 
   return (
     <div>
       <h1>Home</h1>
-      <button onClick={handleClick}>Trigger Error</button>
+     
     </div>
   );
 };
